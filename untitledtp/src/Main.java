cc442a660eb639670262633f66c28b2347b93029
reilbrian <126490904+reilbrian@users.noreply.github.com>
2023-04-06 17:15:52 -0300

    Add files via upload
    mejora la deteccion del puntajes del pronostico
    (falta hacer que detecte cuando son pronosticos de diferentes personas)

--- untitledtp/src/Main.java
+++ untitledtp/src/Main.java
@@ -17,78 +17,71 @@ public class Main {
 
         //       lectura de los resultados de los partidos
 
-          String lineares; //string que lee los resultados uno por uno
-          BufferedReader br= new BufferedReader(new FileReader("resultados.csv"));
-          lineares= br.readLine();
-          String[] nombresequipo= new String[100];
-          int[] golesanotados= new int[100];
-          int equiposcant=0;
+        String lineares; //string que lee los resultados uno por uno
+        BufferedReader br = new BufferedReader(new FileReader(resultados));
+        lineares = br.readLine();
+        String[] nombresequipo = new String[100];
+        int[] golesanotados = new int[100];
+        int equiposcant = 0;
 
-          for(int i=0;(lineares= br.readLine())!=null;i++) {
-              String[] posicion = lineares.split(";");
+        for (int i = 0; (lineares = br.readLine()) != null; i++) {
+            String[] posicion = lineares.split(";");
 
-              nombresequipo[i] = posicion[0];
-              golesanotados[i] = Integer.parseInt(posicion[1]);
-              nombresequipo[i+1] = posicion[2];
-              golesanotados[i+1] = Integer.parseInt(posicion[3]);
-              i++;equiposcant=equiposcant+2;
-          }
+            nombresequipo[i] = posicion[0];
+            golesanotados[i] = Integer.parseInt(posicion[1]);
+            nombresequipo[i + 1] = posicion[2];
+            golesanotados[i + 1] = Integer.parseInt(posicion[3]);
+            i++;
+            equiposcant = equiposcant + 2;
+        }
 
-          //formador de equipos
-          equipo equipos[]=new equipo[equiposcant];
-          partido partidos[]=new partido[equiposcant];
-          ronda ronda1 = new ronda();
-          for (int i=0;i<equiposcant;i++){
-              equipos[i]=new equipo(nombresequipo[i],i);
-          }
+        //formador de equipos
+        equipo equipos[] = new equipo[equiposcant];
+        partido partidos[] = new partido[equiposcant];
+        ronda ronda1 = new ronda();
+        for (int i = 0; i < equiposcant; i++) {
+            equipos[i] = new equipo(nombresequipo[i], i);
+        }
 
-          //formador de partidos
-          int j=0;
-          for (int i=0;i<(equiposcant/2);i++){
-              partidos[i]=new partido(equipos[j],equipos[j+1],golesanotados[j],golesanotados[j+1]);
-              ronda1.partidos.add(partidos[i]);
-              j=j+2;
-          }
+        //formador de partidos
+        int j = 0;
+        for (int i = 0; i < (equiposcant / 2); i++) {
+            partidos[i] = new partido(equipos[j], equipos[j + 1], golesanotados[j], golesanotados[j + 1]);
+            ronda1.partidos.add(partidos[i]);
+            j = j + 2;
+        }
 
-         // lectura del pronostico de los partidos
+        // lectura del pronostico de los partidos
 
         String lineapro;
-        BufferedReader br1= new BufferedReader(new FileReader("pronostico.csv"));
-        String[] resultadospro = new String[6];
-        lineapro= br1.readLine();
+        BufferedReader br1 = new BufferedReader(new FileReader(pronostico));
+        String[] resultadospro = new String[100];
+        String[] nombres=new String[100];
+        lineapro = br1.readLine();
+        int proncant = 0;
 
-        for(int i=0;(lineapro = br1.readLine())!=null;i++) {
+        for (int i = 0; (lineapro = br1.readLine()) != null; i++) {
 
-            String[] posicion2 = lineapro.split(";");
+            String[] posicion = lineapro.split(";");
 
-            resultadospro[i] = posicion2[1];
-            resultadospro[i+1] = posicion2[2];
-            resultadospro[i+2] = posicion2[3];
-            i=i+2;
+            resultadospro[i] = posicion[1];
+            resultadospro[i + 1] = posicion[2];
+            resultadospro[i + 2] = posicion[3];
+            i = i + 2;
+            proncant++;
         }
 
         ronda1.ListarGanadores();
+        //detecta los pronosticos
+        int puntaje = 0;
+        j=0;int z=0; String aux=nombres[0];
 
-        int puntaje=0;
-
-       if (golesanotados[0]>golesanotados[1] && resultadospro[0].equals("x")){
-           puntaje++;
-       }
-       if (golesanotados[1]>golesanotados[0] && resultadospro[2].equals("x")){
-           puntaje++;
-       }
-       if (golesanotados[0]==golesanotados[1] && resultadospro[1].equals("x")){
-           puntaje++;
-       }
-       if (golesanotados[2]>golesanotados[3] &&  resultadospro[3].equals("x")){
-           puntaje++;
-       }
-      if (golesanotados[3]>golesanotados[2] && resultadospro[5].equals("x")){
-           puntaje++;
-       }
-      if (golesanotados[2]==golesanotados[3] && resultadospro[4].equals("x")){
-           puntaje++;
-       }
-        System.out.println("sumaste :"+puntaje+" puntos con tu pronostico ");
+        for (int i = 0; i <proncant; i++) {
+            if (golesanotados[j] >  golesanotados[j+1] && resultadospro[z].equals("x"))   {puntaje++;}
+            if (golesanotados[j] == golesanotados[j+1] && resultadospro[z+1].equals("x")) {puntaje++;}
+            if (golesanotados[j] <  golesanotados[j+1] && resultadospro[z+2].equals("x")) {puntaje++;}
+            j=j+2;z=z+3;
+        }
+            System.out.println("sumaste :" + puntaje + " puntos con tu pronostico ");
     }
 }
--- untitledtp/src/ronda.java
+++ untitledtp/src/ronda.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 public class ronda {
-    partido[] partidoss = new partido[4];
-
     public ArrayList<partido> partidos = new ArrayList<partido>();
     public void ListarGanadores()
     {
